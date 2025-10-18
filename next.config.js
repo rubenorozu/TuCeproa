55c040cd5b80429828aa219a6447a9f1a56eb213
rubenoroz <rubenoroz@gmail.com>
2025-10-17 22:38:32 -0600

    fix: Excluido public/uploads del bundle de funciones Serverless en next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  /* config options here */
+  // Añadir esta configuración para excluir public/uploads del bundle de las funciones Serverless
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [
+        ...config.externals,
+        {
+          'public/uploads': 'public/uploads', // Excluir el directorio public/uploads
+        },
+      ];
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
