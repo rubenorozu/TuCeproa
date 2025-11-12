619c74d6228f22688ea9f57ecf2eb3aba3d0454b
univa <univa@Ruben-Oroz-Mac-Studio.local>
2025-11-12 09:56:45 -0600

    redeploy

--- app/api/admin/recurring-blocks/[id]/route.ts
+++ app/api/admin/recurring-blocks/[id]/route.ts
@@ -52,7 +52,7 @@ export async function PUT(request: Request, { params }: { params: { id: string }
 export async function DELETE(request: Request, { params }: { params: { id: string } }) {
   const session = await getServerSession();
 
-  const allowedRoles = [Role.SUPERUSER, Role.ADMIN_RESOURCE, Role.ADMIN_RESERVATION];
+  const allowedRoles: Role[] = [Role.SUPERUSER, Role.ADMIN_RESOURCE, Role.ADMIN_RESERVATION];
   if (!session || !allowedRoles.includes(session.user.role)) {
     return NextResponse.json({ error: 'Acceso denegado.' }, { status: 403 });
   }
